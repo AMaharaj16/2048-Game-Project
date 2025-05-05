e8cb8fb650a4852f24f51d9bf53a15f266a9bc33
AMaharaj16 <aayush.maharaj16@gmail.com>
2025-05-05 11:14:12 -0600

    New file created to actually run the game.

---
+++ game_2048.py
@@ -0,0 +1,29 @@
+import logic_2048
+
+if __name__ == '__main__':
+    mat = logic_2048.start_game()
+    print(mat)
+
+state = 'GAME ONGOING'
+
+while state == 'GAME ONGOING':
+    move = input("Next Move:")
+    if move == 'w':
+        mat = logic_2048.move_up(mat)
+        mat = logic_2048.random_two(mat)
+    
+    if move == 's':
+        mat = logic_2048.move_down(mat)
+        mat = logic_2048.random_two(mat)
+    
+    if move == 'a':
+        mat = logic_2048.move_left(mat)
+        mat = logic_2048.random_two(mat)
+
+    if move == 'd':
+        mat = logic_2048.move_right(mat)
+        mat = logic_2048.random_two(mat)
+
+    state = logic_2048.game_state(mat)
+    print(mat)
+    print(state)
